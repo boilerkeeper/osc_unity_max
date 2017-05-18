9bfb4fb6f4c766198a00a36ded224aa31d9a94b5
boilerkeeper <simonwright81@gmail.com>
2017-05-18 14:30:28 +0100

    First Commit
    Added -
    Unity Project: OSC_LIBRARY_TEST
    Max Patch: MaxOscToMidi

---
+++ OSC_LIBRARY_TEST/Assets/oscComms.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SharpOSC;
+
+public class oscComms : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		var message = new SharpOSC.OscMessage ("/note", 60, 100, 1000);
+		var sender = new SharpOSC.UDPSender ("127.0.0.1", 9000);
+		sender.Send (message);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
